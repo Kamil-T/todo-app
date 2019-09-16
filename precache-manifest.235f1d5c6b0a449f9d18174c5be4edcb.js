fba17ef9fd61ebc3c2533bfe6fe6294ec8c90a4e
Kamil T <trzaskomakamil@gmail.com>
2019-09-16 17:13:45 +0200

    Updates

---
+++ precache-manifest.235f1d5c6b0a449f9d18174c5be4edcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cd8405a4dd4d2a40124b7953ab7465e",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "8e7988802983514e8137",
+    "url": "/todo-app/static/css/main.3c205d2c.chunk.css"
+  },
+  {
+    "revision": "f83408e511d47ca15982",
+    "url": "/todo-app/static/js/2.b074852e.chunk.js"
+  },
+  {
+    "revision": "8e7988802983514e8137",
+    "url": "/todo-app/static/js/main.e54d95e4.chunk.js"
+  },
+  {
+    "revision": "02f6bf75ca3b2d88784d",
+    "url": "/todo-app/static/js/runtime~main.c7cb45b0.js"
+  }
+]);
